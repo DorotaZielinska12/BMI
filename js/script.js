326c666536ab5149f4ed52980407c183f4111fb8
Dorota Zielinska <dodamad3@gmail.com>
2023-08-14 18:22:50 +0100

    js work and is ready to calculate bmi

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const heightElement = document.querySelector(".js-height");
+const weightElement = document.querySelector(".js-weight");
+const formElement = document.querySelector(".js-form");
+const bmiElement = document.querySelector(".js-bmi");
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let height = heightElement.value;
+    let weight = weightElement.value;
+
+    let bmi = weight / ((height / 100) ** 2);
+
+    console.log(bmi);
+
+    bmiElement.innerText = bmi.toFixed(2);
+});
+
